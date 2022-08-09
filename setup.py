031389424f1b1cd953fff5fabfca4c28bdbf5279
akash-soni <37892453+akash-soni@users.noreply.github.com>
2022-08-09 13:22:40 +0530

    basic structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ner",
+    version="0.0.1",
+    author="Akash soni",
+    author_email="akash.200287@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
